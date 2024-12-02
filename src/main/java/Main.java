012d1b6f6b749e158a5dde1b380cf20edb8479b0
MashaBerg <mashabregman1974@gmail.com>
2024-12-02 14:21:51 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class hw_Lists {
 
-	public static Node exe1 (int x, int y)
+	public static Node exe1 (int x, int y, int n)
 	{
 		Node <Integer> first = new Node <> ((int)(Math.random()*(y - x + 1 )) + x);
 		Node <Integer> current = first;
 		Node <Integer> next ;
 		
-		for (int i = 1 ; i < 20 ; i++)
+		for (int i = 1 ; i < n ; i++)
 		{
 			next = new Node <> ((int)(Math.random()*(y - x + 1 )) + x);
 			current.setNext(next);
@@ -35,7 +35,7 @@ public class hw_Lists {
 	
 	public static void main(String[] args) {
 		
-		Node <Integer> exe1_list = exe1(3,4);
+		Node <Integer> exe1_list = exe1(3, 4, 5);
 		System.out.println(exe1_list);
 		System.out.print(exe2(4, exe1_list));
 
